8da339d5d8ba27724ff6730dc415b7fa77fcf5f4
helondeng <dhlwust@163.com>
2015-10-21 18:56:45 +0800

    Update iconfont.js

--- src/iconfont.js
+++ src/iconfont.js
@@ -9,38 +9,38 @@ var fs = require('fs'),
 
 
 // 十进制 转 16进制
-function decimal2Hex(n){
+function decimal2Hex(n) {
     var hex = n.toString(16);
     hex = '000'.substr(0, 3 - hex.length) + hex;
     return hex;
 }
 
 // 生成 icon 对应的 content
-function generateIconContent(n){
+function generateIconContent(n) {
     return '&#xf' + decimal2Hex(n);
 }
 
 function mkdir(dir) {
-    if(!fs.existsSync(dir)) {
+    if (!fs.existsSync(dir)) {
         mkdir(path.dirname(dir));
         fs.mkdirSync(dir);
     }
 }
 
 /*
-* generate font files
-* 字体文件 md5
-*/
-exports.genarateFonts = function (opt, icons) {
+ * generate font files
+ * 字体文件 md5
+ */
+exports.genarateFonts = function(opt, icons) {
     var svgPath = opt._svgPath,
         // output = opt.fontsOutput,
         font = fontCarrier.create(),
         svgsObj = {},
         filePath,
         iconContent;
-    icons.forEach(function(icon, index){
+    icons.forEach(function(icon, index) {
         filePath = path.join(svgPath, icon + '.svg');
-        if(fs.existsSync(filePath)) {
+        if (fs.existsSync(filePath)) {
             iconContent = generateIconContent(index);
             svgsObj[iconContent] = fs.readFileSync(filePath).toString();
         } else {
@@ -61,10 +61,10 @@ exports.genarateFonts = function (opt, icons) {
 };
 
 /*
-* 根据icon生成对应的字体
-* 需要判断实际的svg（正则匹配到了不是icon的表达式）文件是否存在，否则会有多余样式
+ * 根据icon生成对应的字体
+ * 需要判断实际的svg（正则匹配到了不是icon的表达式）文件是否存在，否则会有多余样式
  */
-exports.generateCss = function (opt, iconNames, start, step) {
+exports.generateCss = function(opt, iconNames, start, step) {
     var self = this,
         pseudoClass = ~['after', 'before'].indexOf(opt.pseClass) ? opt.pseClass : 'after',
         start = start || 0,
@@ -78,19 +78,50 @@ exports.generateCss = function (opt, iconNames, start, step) {
     content.push('font-family: "mfont";');
     content.push('src: url("' + opt._fontCdn.eot + '");'); // ie9
     content.push('src: url("' + opt._fontCdn.eot + '?#iefix") format("embedded-opentype"),'); // ie6-8
-    content.push('url("' + opt._fontCdn.woff + '") format("woff"),');  // chrome、firefox
+    content.push('url("' + opt._fontCdn.woff + '") format("woff"),'); // chrome、firefox
     content.push('url("' + opt._fontCdn.ttf + '") format("truetype");}'); // chrome、firefox、opera、Safari, Android, iOS 4.2+
 
     content.push('.icon-font{font-family:"mfont";font-size:16px;font-style:normal;font-weight: normal;font-variant: normal;text-transform: none;line-height: 1;position: relative;-webkit-font-smoothing: antialiased;}');
-    iconNames.forEach(function(iconName){
+    iconNames.forEach(function(iconName) {
         iconContent = generateIconContent(start++);
-        if (typeof iconContent !== 'undefined' && fs.existsSync(path.join(opt._svgPath, iconName + '.svg')) ) {
+        if (typeof iconContent !== 'undefined' && fs.existsSync(path.join(opt._svgPath, iconName + '.svg'))) {
             iconContent = iconContent.replace('&#xf', '\\f');
             content.push('.i-' + iconName + ':' + pseudoClass + '{content: "' + iconContent + '";}');
         }
     });
     return content.join('\r\n');
 };
+
+
+function file2Base64(str) {
+    return new Buffer(str).toString('base64');
+}
+
+
+exports.generateBase64Css = function(opt, iconNames, ttf, start, step) {
+    var fontBase64 = file2Base64(ttf);
+
+    var self = this,
+        pseudoClass = ~['after', 'before'].indexOf(opt.pseClass) ? opt.pseClass : 'after',
+        start = start || 0,
+        step = step || 1;
+        
+    var content = [],
+        iconContent;
+    content.push('@font-face { ');
+    content.push('font-family: "iconfont";');
+    content.push('src: url("data:application/octet-stream;base64,' + fontBase64 + '") format("truetype");}');
+
+    content.push('.icon-font{font-family:"mfont";font-size:16px;font-style:normal;font-weight: normal;font-variant: normal;text-transform: none;line-height: 1;position: relative;-webkit-font-smoothing: antialiased;}');
+    iconNames.forEach(function(iconName) {
+        iconContent = generateIconContent(start++);
+        if (typeof iconContent !== 'undefined' && fs.existsSync(path.join(opt._svgPath, iconName + '.svg'))) {
+            iconContent = iconContent.replace('&#xf', '\\f');
+            content.push('.i-' + iconName + ':' + pseudoClass + '{content: "' + iconContent + '";}');
+        }
+    });
+    return content.join('\r\n');
+}
 
 
 exports.exportCssFile = function(iconNames, pseClass, path) {
